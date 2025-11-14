92cd45729d81931448dc5ad739eb462cc240176b
Elizabeth Marcoux <liz.marco12321@gmail.com>
2025-11-13 19:23:30 -0600

    Fixed Customer model associations.

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,5 +1,8 @@
 class Customer < ApplicationRecord
-  has_many :carts, :addresses, :payment_methods, :orders
+  has_many :carts
+  has_many :addresses
+  has_many :payment_methods
+  has_many :orders
 
   validates :name, :email_address, :phone_number, :password_hash, presence: true, uniqueness: true
   validates :phone_number, numericality: true
