bf91bfd34b335f9a1888b68cc16fce742354748a
Elizabeth Marcoux <liz.marco12321@gmail.com>
2025-11-14 05:24:30 -0600

    Created home page with display of all products, with the ability to sort products by their category.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,15 @@
 class HomeController < ApplicationController
   def index
-    
+    @products = Product.where(is_active: true).includes(:category)
+    @products = Product.page(params[:page]).per(32)
+
+    @categories = Category.order(:name)
+
+    if params[:category].present?
+      @selected_category = Category.find_by(id: params[:category])
+      @products = Product.where(category_id: @selected_category.id).page(params[:page])
+    else
+      @products = Product.page(params[:page])
+    end
   end
 end
