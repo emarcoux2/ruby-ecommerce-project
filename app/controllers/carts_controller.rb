f672a9e420f565753bb7f537237c99f002944e8d
Elizabeth Marcoux <liz.marco12321@gmail.com>
2025-11-20 15:17:46 -0600

    Configured CartsController and Stripe initializer file.

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,7 +1,26 @@
 class CartsController < ApplicationController
-  def index
+  def add
+    session[:cart] << params[:product_id]
+    flash[:notice] = "Item added to cart!"
   end
 
-  def show
+  def quantity
+    product_id = params[:product_id]
+    quantity = params[:quantity].to_i
+
+    if session[:cart][product_id].nil?
+      flash[:notice] = "Product not found in cart."
+    end
+
+    if quantity <= 0
+      flash[:notice] = "Invalid quantity."
+    end
+
+    session[:cart][product_id]["quantity"] = quantity
+  end
+
+  def destroy
+    session[:cart] = {}
+    flash[:notice] = "Nothing in your cart! Start shopping!"
   end
 end
