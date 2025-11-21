3aa3cd8b2a5d89e940ef8d9faa30c19b13bafc71
Elizabeth Marcoux <liz.marco12321@gmail.com>
2025-11-20 23:14:04 -0600

    Initialized cart functionality, defined actions in the Carts controller, set up the cart show page, and modified cart routes.

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,7 +1,53 @@
 class CartsController < ApplicationController
-  def index
+  before_action :initialize_cart
+
+  def add
+    product_id = params[:product_id].to_s
+    quantity = (params[:quantity] || 1).to_i
+
+    product = Product.find(product_id)
+
+    session[:cart] ||= {}
+
+    session[:cart][product_id] ||= 0
+    session[:cart][product_id] += quantity
+
+    flash[:notice] = "#{product.name} was added to your cart."
+    redirect_back fallback_location: products_path
+  end
+
+  def update
+    product_id = params[:product_id].to_s
+    quantity = params[:quantity].to_i
+
+    if quantity > 0
+      @cart[product_id] = quantity
+    else
+      @cart.delete(product_id)
+    end
+
+    session[:cart] = @cart
+    redirect_to cart_path, notice: "Cart has been updated."
+  end
+
+  def destroy
+    product_id = params[:product_id].to_s
+
+    @cart.delete(product_id)
+    session[:cart] = @cart
+
+    redirect_to cart_path, notice: "Product removed from cart!"
   end
 
   def show
+    @cart_products = Product.where(id: @cart.keys)
+  end
+
+  private
+
+  def initialize_cart
+    session[:cart] ||= {}
+    @cart = session[:cart]
+    @cart_count = @cart.values.sum
   end
 end
