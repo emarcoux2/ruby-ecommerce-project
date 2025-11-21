002d450cb0eeb7f2d8033b13d3d7da41a27b0f50
Elizabeth Marcoux <liz.marco12321@gmail.com>
2025-11-21 01:03:27 -0600

    Modified Customer model.

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,27 +1,19 @@
 class Customer < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
   has_many :carts
   has_many :addresses
   has_many :payment_methods
   has_many :orders
 
-  validates :name, :email_address, :phone_number, :password_hash, presence: true
+  validates :name, :email_address, :phone_number, presence: true
 
-  attr_writer :login
-  def login
-    @login || self.email
+  def email
+    self.email_address
   end
 
-  def self.find_for_database_authentication(warden_conditions)
-    conditions = warden_conditions.dup
-
-    if login = conditions.delete(:login)
-      where(conditions).where(
-        [ "lower(email) = :value", { value: login.downcase } ]
-      ).first
-    else
-      where(conditions).first
-    end
+  def email=(value)
+    self.email_address = value
   end
 end
