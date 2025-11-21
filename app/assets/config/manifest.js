683aa6d5d4a41312d11aa6059253e156cb7c373b
Elizabeth Marcoux <liz.marco12321@gmail.com>
2025-11-21 12:23:28 -0600

    Configured log in functionality. Able to create an account, log out of the account, and displays the name of the logged in user.

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,3 +1,7 @@
 // = link_tree ../images
+// = link application.js
+// = link controllers/application.js
+// = link controllers/index.js
+// = link controllers/hello_controller.js
 // = link_directory ../javascripts .js
 // = link_directory ../stylesheets .css
