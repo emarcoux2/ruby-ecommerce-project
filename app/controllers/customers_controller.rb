f3763dfa4b75a0bfadafbd0220f5cc9434d355dc
Elizabeth Marcoux <liz.marco12321@gmail.com>
2025-12-12 16:00:20 -0600

    Modified customers controller and address model to display basic account information like username, primary address, and most recent order.

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -3,5 +3,10 @@ class CustomersController < ApplicationController
   end
 
   def show
+    @customer = current_customer
+    # Fetch the primary address
+    @primary_address = @customer.addresses.find_by(is_primary: true)
+    # Fetch most recent order (optional)
+    @recent_order = @customer.orders.order(order_date: :desc).first
   end
 end
--- app/models/address.rb
+++ app/models/address.rb
@@ -21,10 +21,18 @@ class Address < ApplicationRecord
 
   belongs_to :customer
 
+  before_save :unset_other_primary, if: :is_primary?
+
   validates :street, :city, :postal_code, :province, presence: true
   validates :is_primary, inclusion: { in: [ true, false ] }
 
   def full_address
     [ street, city, province, postal_code ].compact.join(", ")
+  end
+
+  private
+
+  def unset_other_primary
+    customer.addresses.where.not(id: id).update_all(is_primary: false)
   end
 end
